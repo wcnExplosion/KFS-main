2238177e1557c1e58150885a5f8b2c28577e7a60
wcn <126437530+wcnExplosion@users.noreply.github.com>
2025-08-01 16:36:56 +0800

    Update KFS.py

--- models/KFS.py
+++ models/KFS.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from layers.SelfAttention_Family import FullAttention, AttentionLayer
 from layers.Embed import DataEmbedding_inverted, PositionalEmbedding
 import numpy as np
 from layers.RevIN import RevIN
